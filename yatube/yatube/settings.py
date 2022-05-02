3fb17da7275f3847a307584fa2015650cc67c1f8
Илья Колесниченко <iliakoll@yandex.ru>
2022-05-02 15:27:51 +0300

    Добавил DjDT

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -30,8 +30,17 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
     '[::1]',
     'testserver',
+    'www.ikoll.pythonanywhere.com',
+    'ikoll.pythonanywhere.com',
 ]
 
+
+INTERNAL_IPS = [
+    'localhost',
+    '127.0.0.1',
+    '[::1]',
+    'testserver',
+]
 
 # Application definition
 
@@ -47,6 +56,7 @@ INSTALLED_APPS = [
     'core.apps.CoreConfig',
     'about.apps.AboutConfig',
     'sorl.thumbnail',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -57,6 +67,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'yatube.urls'
